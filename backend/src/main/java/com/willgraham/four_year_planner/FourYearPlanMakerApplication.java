ef72001d71aa93c9a3c96adcec73d1482e44e0ff
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-01-24 00:21:00 -0500

    Added initial project from Spring Initializer

---
+++ backend/src/main/java/com/willgraham/four_year_planner/FourYearPlanMakerApplication.java
@@ -0,0 +1,13 @@
+package com.willgraham.four_year_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourYearPlanMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourYearPlanMakerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/willgraham/four_year_planner/FourYearPlanMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willgraham.four_year_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourYearPlanMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
