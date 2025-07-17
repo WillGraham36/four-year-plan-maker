2c7894a9bc8c329e429e1e3f6abd5102bfddb5f0
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-07-16 23:26:57 -0400

    feat: remove extra table rows from transfer courses in list

--- frontend/src/components/planner/semester.tsx
+++ frontend/src/components/planner/semester.tsx
@@ -10,7 +10,8 @@ interface SemesterProps {
   year: number;
   courses: Course[];
   disableCourseEditing?: boolean;
-  children?: React.ReactNode;
+  isCore?: boolean; // Used to determine if this is a core semester, or a transfer semester
+  title?: React.ReactNode;
 }
 
 const Semester = ({
@@ -18,14 +19,15 @@ const Semester = ({
   year,
   courses,
   disableCourseEditing,
-  children,
+  isCore = true,
+  title,
 }: SemesterProps) => {
   const semesterTerm = termYearToString(term, year);
   
   return (
     <SemesterProvider term={term} year={year} initialCourses={courses}>
       <div className="flex flex-col rounded-lg border w-full h-min overflow-hidden bg-card shadow-md">
-        {children ? children : (
+        {title ? title : (
           <p className="w-full border-b p-1 px-3 text-sm md:text-base">
             {semesterTerm}
           </p>
@@ -37,7 +39,11 @@ const Semester = ({
           <p className="w-full text-center py-1">Credits</p>
         </div>
 
-        <SemesterCourseList initialCourses={courses} disableCourseEditing={disableCourseEditing} />
+        <SemesterCourseList 
+          initialCourses={courses} 
+          disableCourseEditing={disableCourseEditing} 
+          isCore={isCore}
+        />
       </div>
     </SemesterProvider>
   )
@@ -47,17 +53,44 @@ const Semester = ({
  * Keeps track of the number of courses and adds additional CourseInput components as needed
  * Shows total credits as well
  */
-const SemesterCourseList = ({ initialCourses, disableCourseEditing } : { initialCourses?: Course[], disableCourseEditing?: boolean}) => {
+interface SemesterCourseListProps {
+  initialCourses?: Course[];
+  disableCourseEditing?: boolean;
+  isCore?: boolean;
+}
+const SemesterCourseList = ({ initialCourses, disableCourseEditing, isCore } : SemesterCourseListProps) => {
   const initialLength = initialCourses?.length ?? 0;
   const { courses } = useSemester();
   const [numCourseInputs, setNumCourseInputs] = useState<number>(Math.max(initialLength, 5));
 
-  // Ensure that there is alwyas at least one empty CourseInput, with a max of 8
   useEffect(() => {
-    if(courses.length === numCourseInputs && numCourseInputs < 8) {
+    if (!isCore && courses.length === numCourseInputs && numCourseInputs < 8) {
       setNumCourseInputs((prevNum) => prevNum + 1);
     }
-  }, [courses, numCourseInputs])
+  }, [courses, numCourseInputs, isCore]);
+
+  if (isCore) {
+    if (initialLength === 0) {
+      return (
+        <div className="p-3 text-muted-foreground text-sm text-center">
+          No transfer courses available
+        </div>
+      );
+    }
+    return (
+      <>
+        {initialCourses?.map((course) => (
+          <CourseInput key={course.courseId} initialCourse={course} disabled={disableCourseEditing} />
+        ))}
+        <div className="h-8 w-full grid grid-cols-[3fr,3.5rem] text-xs md:text-sm">
+          <p className="w-full flex items-center px-3 text-muted-foreground">Total Credits</p>
+          <p className="w-full flex items-center justify-center">
+            {courses.reduce((total, course) => total + (course.credits || 0), 0)}
+          </p>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -74,7 +107,7 @@ const SemesterCourseList = ({ initialCourses, disableCourseEditing } : { initial
         </p>
       </div>
     </>
-  )
+  );
 }
 
 const SemesterHeaderText = ({ children }: { children: React.ReactNode }) => {
--- frontend/src/components/planner/transfer-credits-container.tsx
+++ frontend/src/components/planner/transfer-credits-container.tsx
@@ -4,11 +4,17 @@ import { Semester, SemesterHeaderText } from './semester'
 
 const TransferCreditsContainer = ({ courses }: {  courses: Course[]}) => {
   return (
-    <Semester term='TRANSFER' year={-1} courses={courses} disableCourseEditing={true}>
-      <SemesterHeaderText>
-        Transfer Credits
-      </SemesterHeaderText>
-    </Semester>
+    <Semester 
+      term='TRANSFER' 
+      year={-1} 
+      courses={courses} 
+      disableCourseEditing={true}
+      title={
+        <SemesterHeaderText>
+          Transfer Credits
+        </SemesterHeaderText>
+      }
+    />
   )
 }
 
