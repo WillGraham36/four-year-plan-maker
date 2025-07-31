e1977d5c25a5adb7d4686a5b69227af919162f87
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-07-30 21:10:24 -0400

    fix: fix hydration issues

--- frontend/src/components/ui/satisfied-check.tsx
+++ frontend/src/components/ui/satisfied-check.tsx
@@ -17,12 +17,14 @@ const SatisfiedCheck = ({
 }: SatisfiedCheckProps) => {
   return (
     <Tooltip delayDuration={1000}>
-      <TooltipTrigger className='text-secondary flex items-center gap-1 cursor-default'>
-        {isSatisfied ? (
-          <CircleCheckBig size={16} className='inline text-green-500' />
-        ) : (
-          <Circle size={16} className='inline' />
-        )}
+      <TooltipTrigger className='text-secondary flex items-center gap-1 cursor-default' asChild>
+        <span>
+          {isSatisfied ? (
+            <CircleCheckBig size={16} className='inline text-green-500' />
+          ) : (
+            <Circle size={16} className='inline' />
+          )}
+        </span>
       </TooltipTrigger>
       <TooltipContent className='text-center'>
         {isSatisfied ? "Requirement satisfied" : message}
