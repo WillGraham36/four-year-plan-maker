22eee5cfd872f3a659488bd25513572929dc88f0
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-08-07 00:03:57 -0400

    chore: update scripts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      {/* <head>
         <script
           crossOrigin="anonymous"
           src="//unpkg.com/react-scan/dist/auto.global.js"
         />
-      </head>
+      </head> */}
       <body
         className={`${outfit.className} antialiased`}
       >
@@ -31,7 +31,7 @@ export default function RootLayout({
           {children}
           <Toaster 
             position="top-center"
-            richColors
+            // richColors
           />
         </Providers>
       </body>
--- frontend/src/components/planner/requirements-context.tsx
+++ frontend/src/components/planner/requirements-context.tsx
@@ -41,7 +41,7 @@ export const RequirementsProvider = ({ children, initialGenEds, initialULCourses
   const router = useRouter();
   useEffect(() => {
     if(!userInfo || !userInfo.startSemester || !userInfo.endSemester) {
-      toast.message("Please complete your account setup to view the planner");
+      toast.info("Please complete your account setup to view the planner");
       router.push("/account/setup");
     }
   }, [userInfo, router]);
