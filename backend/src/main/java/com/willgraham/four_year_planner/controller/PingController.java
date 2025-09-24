76d1df2cb39440161903396effec1d56e3d9cf36
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2025-09-24 18:19:31 -0400

    feat: add db request to ping endpoint to reduce latency

--- backend/src/main/java/com/willgraham/four_year_planner/controller/PingController.java
+++ backend/src/main/java/com/willgraham/four_year_planner/controller/PingController.java
@@ -1,13 +1,19 @@
 package com.willgraham.four_year_planner.controller;
 
+import com.willgraham.four_year_planner.repository.UserRepository;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Data
 public class PingController {
+
+    private final UserRepository userRepository;
 
     @GetMapping("/api/public/ping")
     public String ping() {
+        userRepository.count(); // Trigger DB + repository initialization
         return "pong";
     }
 }
