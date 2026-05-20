ab7ee8de85ac4acb38a522c1961f132984655eae
Will Graham <142061623+WillGraham36@users.noreply.github.com>
2026-05-20 00:40:41 -0400

    feat: add docker files and compose, change nextjs server side functionality to work with docker

--- frontend/src/lib/api/server.ts
+++ frontend/src/lib/api/server.ts
@@ -1,19 +1,20 @@
 import { auth } from "@clerk/nextjs/server";
 import { CustomServerResponse } from "../utils/types";
+import { getApiBaseUrl } from "./getApiBaseUrl";
 
 export const fetchWithAuth = async (
   route: string,
   options: {
-    params?: URLSearchParams,
-    init?: RequestInit,
-  } = {}
+    params?: URLSearchParams;
+    init?: RequestInit;
+  } = {},
 ): Promise<CustomServerResponse<any>> => {
   const { params = new URLSearchParams(), init = {} } = options;
   try {
     const { getToken } = await auth();
     const token = await getToken();
 
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/${route}?${params.toString()}`;
+    const url = `${getApiBaseUrl()}/${route}?${params.toString()}`;
 
     const res = await fetch(url, {
       credentials: "include",
@@ -26,11 +27,16 @@ export const fetchWithAuth = async (
 
     const data = await res.json();
     if (!res.ok) {
+      console.error("API Error:", data);
       return { ok: false, message: data.message, data: null };
     }
 
     return { ok: true, message: data.message, data: data.data };
   } catch (error) {
-    return { ok: false, message: "An error has occured, please try again", data: null}
+    return {
+      ok: false,
+      message: "An error has occured, please try again",
+      data: null,
+    };
   }
 };
